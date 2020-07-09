87ee4cca2da181e26cf6c40cc466f3d9ad12f463
Nathan Chung <chung.nathanh@gmail.com>
2020-07-09 21:05:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,7 +166,7 @@ def player_numbers(team)
   game_hash.each do |location, team_info|
     if team_info[:team_name] == team
       team_info[:players].collect do |players|
-        return players[:number]
+        return players[:number].to_s
       end
   end
   end 
